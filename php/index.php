dacd8f0cfa119f0166f2d241a27eac2d5ddf7d97
Muhammed <2170564@talnet.nl>
2023-05-08 10:28:39 +0200

    first commit

---
+++ php/index.php
@@ -0,0 +1,45 @@
+<?php
+
+$nummer1 = 12;
+$nummer2 = 10;
+
+$antwoord = $nummer1 + $nummer2;
+
+echo "Het antwoord is: " . $antwoord;
+
+?>
+
+<br>
+
+<?php
+
+$nummer1 = 12;
+$nummer2 = 10;
+
+$antwoord = $nummer1 * $nummer2;
+
+echo "Het antwoord is: " . $antwoord;
+
+?>
+
+<br>
+
+<?php
+
+$naam = "Muhammet";
+
+echo "Welkom " . $naam;
+
+?>
+
+<?php
+/*
+// deze code bevat veel fouten
+   haal alle fouten eruit!
+//
+
+$mijnNaam="Carl";
+$jouwNaam="Camel";
+echo $mijnNaam . " zit bij " . $jouwNaam . " in de klas!"
+*/
+?>
