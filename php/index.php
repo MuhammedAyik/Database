8060aed7b62595d6889d8c7af431d0f280fa5459
MuhammedAyik <131653676+MuhammedAyik@users.noreply.github.com>
2023-05-15 09:37:36 +0200

    Update index.php

--- php/index.php
+++ php/index.php
@@ -33,13 +33,4 @@ echo "Welkom " . $naam;
 ?>
 
 <?php
-/*
-// deze code bevat veel fouten
-   haal alle fouten eruit!
-//
-
-$mijnNaam="Carl";
-$jouwNaam="Camel";
-echo $mijnNaam . " zit bij " . $jouwNaam . " in de klas!"
-*/
 ?>
