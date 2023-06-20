4679a911494749e4391e209b8ef998a8047ef7f1
MuhammedAyik <131653676+MuhammedAyik@users.noreply.github.com>
2023-06-20 12:26:34 +0200

    Update delete.php

--- pdo/week7/PDO Delete/delete.php
+++ pdo/week7/PDO Delete/delete.php
@@ -1,5 +1,4 @@
 <?php
-// Maak een verbinding met de database
 $servername = "localhost:3307";
 $username = "root";
 $password = "";
@@ -10,10 +9,8 @@ if ($conn->connect_error) {
     die("Verbinding mislukt: " . $conn->connect_error);
 }
 
-// Ontvang de product_code uit de URL-parameter
 $product_code = $_GET['product_code'];
 
-// Verwijder het product met de opgegeven product_code uit de tabel producten
 $sql = "DELETE FROM producten WHERE product_code = $product_code";
 if ($conn->query($sql) === TRUE) {
     echo "Product met code $product_code is succesvol verwijderd.";
@@ -21,6 +18,5 @@ if ($conn->query($sql) === TRUE) {
     echo "Fout bij het verwijderen van het product: " . $conn->error;
 }
 
-// Sluit de databaseverbinding
 $conn->close();
 ?>
